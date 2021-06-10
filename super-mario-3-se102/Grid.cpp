f2b6be1442d8dfa8c91b0cc7a596d9ea3a101d5a
CMB <thaichibao@gmail.com>
2021-06-10 22:10:13 +0700

    update Grid

--- super-mario-3-se102/Grid.cpp
+++ super-mario-3-se102/Grid.cpp
@@ -1,21 +1,27 @@
 #include "Grid.h"
 
-Grid::Grid(int numberOfColumns, int numberOfRows)
-	: numOfCols(numberOfColumns), numOfRows(numberOfRows),
-	cells(std::vector<std::list<LPEntity>*>(numberOfRows* numberOfColumns))
+Grid::Grid(int numberOfColumns, int numberOfRows, Utils::Dimension cellSize)
+	: numOfCols(numberOfColumns), numOfRows(numberOfRows), cellSize(cellSize),
+	cells(std::vector<LPEntitiesInCell>(numberOfRows* numberOfColumns))
 {
 	for (auto& cell : cells)
 		cell = new std::list<LPEntity>();
 
 }
 
-void Grid::Add(LPEntity entity, int column, int row)
+
+void Grid::Add(LPEntity entity, const Utils::Vector2& cellIndex)
 {
-	cells[row * column + column]->push_back(entity);
+	cells[cellIndex.y * cellIndex.x + cellIndex.x]->push_back(entity);
 }
 
-const std::list<LPEntity>* Grid::EntitiesAt(LPEntity entity, int column, int row)
+const LPEntitiesInCell Grid::EntitiesAt(const Utils::Vector2& cellIndex)
 {
-	return cells[column * row + column];
+	return cells[cellIndex.y * cellIndex.x + cellIndex.x];
+}
+
+Utils::Vector2 Grid::GetCellIndexAtPoint(const Utils::Vector2& point)
+{
+	return Utils::Vector2(point.x / cellSize.width, point.y / cellSize.height);
 }
 
--- super-mario-3-se102/Grid.h
+++ super-mario-3-se102/Grid.h
@@ -1,18 +1,24 @@
 #pragma once
 #include <list>
+#include <unordered_map>
 #include "Entity.h"
+#include "Utils.h"
 
+typedef std::list<LPEntity>* LPEntitiesInCell;
 //reference: https://gameprogrammingpatterns.com/spatial-partition.html
+//Basic Spatial Grid, treat each entity at a point, do not take into account it's size
 class Grid
 {
 public:
-	Grid(int numberOfCollumns, int numberOfRows);
-	void Add(LPEntity entity, int column, int row);
-	const std::list<LPEntity>* EntitiesAt(LPEntity entity, int column, int row);
+	Grid(int numberOfCollumns, int numberOfRows, Utils::Dimension cellSize);
+	virtual void Add(LPEntity entity, const Utils::Vector2& cellIndex);
+	const LPEntitiesInCell EntitiesAt(const Utils::Vector2& cellIndex);
+	Utils::Vector2 GetCellIndexAtPoint(const Utils::Vector2& point);
 protected:
 	//each cell contains a list of entities
-	std::vector<std::list<LPEntity>*> cells;
+	std::vector<LPEntitiesInCell> cells;
 	int numOfRows;
 	int numOfCols;
+	Utils::Dimension cellSize;
 };
 
