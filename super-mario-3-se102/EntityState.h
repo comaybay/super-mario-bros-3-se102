0c750a4cc2d645d58c7e3638d4c6d7b583040d5d
CMB <thaichibao@gmail.com>
2021-06-15 22:35:22 +0700

    EntityState

---
+++ super-mario-3-se102/EntityState.h
@@ -0,0 +1,35 @@
+#pragma once
+
+/// <summary>
+/// type used for storing an entity's method pointer, use this implement basic finite state machine.
+/// </summary>
+template<class ENTITY>
+class EntityState
+{
+public:
+	typedef ENTITY* LPENTITY;
+	using Handler = void (ENTITY::*)(float);
+
+	EntityState(LPENTITY entity);
+	void SetHandler(Handler handler);
+	void Handle(float delta);
+
+private:
+	EntityState<ENTITY>::Handler stateHandler;
+	LPENTITY handlerThis;
+};
+
+template<class ENTITY>
+inline EntityState<ENTITY>::EntityState(LPENTITY entity) {
+	handlerThis = entity;
+}
+
+template<class ENTITY>
+inline void EntityState<ENTITY>::SetHandler(Handler handler) {
+	stateHandler = handler;
+}
+
+template<class ENTITY>
+inline void EntityState<ENTITY>::Handle(float delta) {
+	(handlerThis->*stateHandler)(delta);
+}
