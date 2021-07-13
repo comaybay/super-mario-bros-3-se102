045af6206bda6048d8c6f77b35f93f4e910452a9
CMB <thaichibao@gmail.com>
2021-07-13 14:10:38 +0700

    adjust MAX_FALL_SPEED

--- super-mario-3-se102/EntityConstants.cpp
+++ super-mario-3-se102/EntityConstants.cpp
@@ -1,5 +1,5 @@
 #include "EntityConstants.h"
 
-const float EntityConstants::MAX_FALL_SPEED = 350.0f;
+const float EntityConstants::MAX_FALL_SPEED = 250.0f;
 const float EntityConstants::GRAVITY = 785;
 const float EntityConstants::GRAVITY_STRONG = 1850;
